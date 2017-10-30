2fffd3491fcab354d25030481b7d7c4cf8565c90
turtlexuan <turtlexuan@urclass.co>
2017-10-30 14:33:58 +0800

    Initial Commit

---
+++ Network-Evolution-Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Network-Evolution-Practice
+//
+//  Created by urclass on 2017/10/30.
+//  Copyright © 2017年 turtlexuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
